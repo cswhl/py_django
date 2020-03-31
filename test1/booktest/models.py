e25d8a2e69bca9940b5d0fa04036457fdf2724e5
cswhl <cswhltime@126.com>
2020-03-31 02:36:04 +0000

    add source

--- test1/booktest/models.py
+++ test1/booktest/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+# 设计和表对应的类、模型类
 # Create your models here.
+
+# 图书类
+
+
+class BookInfo(models.Model):
+    '''图书模型类'''
+    btitle = models.CharField(max_length=20)
+    bpub_date = models.DateField()
+
+
+class HeroInfo(models.Model):
+    '''英雄类'''
+    hname = models.CharField(max_length=20)
+    hgender = models.BooleanField()
+    hcommtent = models.CharField(max_length=100)
+    hbook = models.ForeignKey('BookInfo')
