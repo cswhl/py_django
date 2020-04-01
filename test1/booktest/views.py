80f12f20f0df41c8922359d6d4e26cb7c6339250
cswhl <cswhltime@126.com>
2020-04-01 08:31:25 +0000

    views.py template path debug

--- test1/booktest/views.py
+++ test1/booktest/views.py
@@ -6,6 +6,6 @@ from django.template import loader,RequestContext
 
 def index(request):
     # return HttpResponse("欢迎光临!")
-    template = loader.get_template('booktest/template')
+    template = loader.get_template('booktest/index.html')
     context = RequestContext(request,{'title':'图书列表', 'list':range(10)})
     return HttpResponse(template.render(context))
