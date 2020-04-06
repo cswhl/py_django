cc8f41528785850d72f06f2c9c040112a734453b
cs <cswhltime@126.com>
2020-04-06 22:28:02 +0800

    增加模块、视图

--- test1/booktest/views.py
+++ test1/booktest/views.py
@@ -3,9 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader,RequestContext
+from django.shortcuts import render
+from booktest.models import BookInfo
 
 def index(request):
     # return HttpResponse("欢迎光临!")
-    template = loader.get_template('booktest/index.html')
-    context = RequestContext(request,{'title':'图书列表', 'list':range(10)})
-    return HttpResponse(template.render(context))
+    #template = loader.get_template('booktest/index.html')
+    #context = RequestContext(request,{'title':'图书列表', 'list':range(10)})
+    #return HttpResponse(template.render(context))
+    return render(request, 'booktest/index.html', {'title':'图书列表', 'list':range(10)})
+
+def detail(request, bid):
+#     根据图书id获得图书
+    book = BookInfo.object
+
+
+
+BookInfo.object
